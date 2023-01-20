3265d3ac00e37da0e06c310dfa972113ae880933
06fitri <83494337+06fitri@users.noreply.github.com>
2023-01-20 07:31:33 +0700

    Add files via upload

---
+++ page2.php
@@ -0,0 +1,35 @@
+<CENTER>
+<table border="1px" width="70%" cellpadding="5" cellspacing="0">
+<tr>
+	<th>No :</th>
+	<th>Nama :</th>
+	<th>Jabatan</th>
+	<th>NO Hp:</th>
+</tr>
+<tr>
+	<td>1.</td>
+	<td>FITRI LESTARI</td>
+	<td>DIREKTUR UTAMA</td>
+	<td>085758579645</td>
+</tr>
+<tr>
+	<td>2.</td>
+	<td>SINDY</td>
+	<td>WAKIL DIREKTUR</td>
+	<td>081237785947</td>
+</tr>
+<tr>
+	<td>3.</td>
+	<td>MARCELINA</td>
+	<td>SEKRETARIS</td>
+	<td>087362816365</td>
+</tr>
+<tr>
+	<td>3.</td>
+	<td>KURNIA</td>
+	<td>BENDAHARA</td>
+	<td>085827624575</td>
+</tr>
+
+ </table>
+</CENTER>
